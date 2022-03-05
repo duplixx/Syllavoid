ef8248a50be578595e2192acb74463a528dc0e07
SHEKHAR PATEL <90516956+duplixx@users.noreply.github.com>
2022-03-05 23:30:18 +0530

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,30 @@
+const countries = [
+    {name: 'Galgotias College'},
+    {name: 'Sharda University'},
+    {name: 'Argentina'},
+    {name: 'Galgotias'}
+  ];
+  
+  const searchInput = document.querySelector('.search-input');
+  const suggestionsPanel = document.querySelector('.suggestions');
+  
+  searchInput.addEventListener('keyup', function() {
+    const input = searchInput.value;
+    suggestionsPanel.innerHTML = '';
+    const suggestions = countries.filter(function(country) {
+      return country.name.toLowerCase().startsWith(input);
+    });
+    suggestions.forEach(function(suggested) {
+      const div = document.createElement('div');
+      div.innerHTML = suggested.name;
+      suggestionsPanel.appendChild(div);
+    });
+    if (input === '') {
+      suggestionsPanel.innerHTML = '';  
+    }
+  })
+
+  
+  
+  
+  
