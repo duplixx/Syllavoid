208eff3fe36acd7311da5a1f5446adbf18293d4b
duplixx <90516956+duplixx@users.noreply.github.com>
2022-03-09 06:37:51 +0530

    now

---
+++ js/search.js
@@ -0,0 +1,41 @@
+const countries = [
+    {name: 'Gcet'},
+    {name: 'Sharda University'},
+    {name: 'Amity University'},
+    {name: 'Galgotias'},
+    
+  ];
+  
+  const searchInput = document.querySelector('.search-input');
+
+  const suggestionsPanel = document.querySelector('.suggestions');
+  suggestionsPanel.style.display = 'none'
+  searchInput.addEventListener('keyup', function() {
+    const input = searchInput.value;
+    suggestionsPanel.innerHTML = '';
+    const suggestions = countries.filter(function(country) {
+      return country.name.toLowerCase().startsWith(input);
+    });
+    
+    // append su
+    suggestions.forEach(function(suggested) {
+      suggestionsPanel.style.display = 'block'
+      const div = document.createElement('a');
+      div.href = `js/${suggested.name}.html`
+      div.innerHTML = suggested.name;
+      div.classList.add("suggested");
+
+      // dev.addEventListener('')
+      suggestionsPanel.appendChild(div);
+    });
+
+    if (input === '') {
+      suggestionsPanel.innerHTML = '';  
+    }
+  })
+
+  
+function whenClick(){
+  const searchInput = document.querySelector('.search-input');
+  
+}
